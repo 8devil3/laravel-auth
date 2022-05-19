7f7bd3390bc2a4448a0148e70be7ab76d5d329f9
8devil3 <8dev.il3@gmail.com>
2022-05-19 19:11:02 +0200

    completata procedura di autenticazione

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome')->name('landingPage');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+      ->namespace('Admin')
+      ->name('admin.')
+      ->prefix('admin')
+      ->group(function () {
+            Route::get('/', 'HomeController@index')->name('home');
+            Route::post('/slugger', 'HomeController@slugger')->name('slugger');
+            Route::resource('/posts', 'PostController');
+      });
+
+
+Route::get('{any?}', function() {
+   return view('guests.landing');
+})->where('any', '.*');
